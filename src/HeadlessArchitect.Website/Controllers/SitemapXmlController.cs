e4c7df33f5dc4af3bcd3e00c07b099190743533d
commodore73 <git@c73.me>
2021-07-08 11:52:46 -0700

    Sitemap XML generator

---
+++ src/HeadlessArchitect.Website/Controllers/SitemapXmlController.cs
@@ -0,0 +1,83 @@
+﻿using Deliverystack.DeliveryApi.Models;
+using Deliverystack.Models;
+using HeadlessArchitect.Website.Controls;
+
+namespace HeadlessArchitect.Website.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+//    [Route("[controller]")]
+
+// ControllerBase?
+
+    public class SitemapXmlController : Controller
+    {
+        private PathApiClient _client;
+        
+        public SitemapXmlController(PathApiClient client)
+        {
+            _client = client;
+        }
+
+        //TODO: get from siteconfig
+        //TODO: cache output until publish
+        private string baseUrl = "https://localhost:44305";
+
+            if (!entry.PageData.ExcludeFromSitemap)
+            {
+                double priority;
+
+                if (entry.PageData.Priority > 0)
+                {
+                    priority = entry.PageData.Priority.Value;
+                }
+                else
+                {
+                    /// 1.0, .9, .8...
+                    priority = 1 - (.1 * ((entry.Url.Length - entry.Url.Length.Replace("/", "")) - 1));
+                }
+
+                if (priority < 0)
+                {
+                    priority = 0;
+                }
+
+                ChangeFrequency changeFrequency = ChangeFrequency.Never;
+
+                if (entry.PageData.ChangeFrequency.HasValue)
+                {
+                    changeFrequency = entry.PageData.ChangeFrequency.Value;
+                }
+
+                _siteMapBuilder.AddUrl(
+                    baseUrl + webpageEntry.Url,
+                    webpageEntry.UpdatedAt, 
+                    changeFrequency,
+                    priority);
+
+                foreach (Tuple<string, string> pair in _repository.GetChildIdentifiers(webpageEntry.EntryUid))
+                {
+                    Recurse(_repository.Get<WebpageEntry>(pair.Item1, pair.Item2), level + 1);
+                }
+            }
+        }
+
+        public async Task<ActionResult> SitemapAsync()
+        {
+            SitemapXmlBuilder siteMapBuilder = new SitemapXmlBuilder();
+
+            foreach (PathApiEntryModel entry in _client.Get(
+                new PathApiBindingModel() {Path = "/", Descendants = 3}).Entries)
+            {
+                if (entry.PageData.IncludeInXmlSiteMap)
+                {
+                    siteMapBuilder.AddUrl(entry.Url, entry.Modified, entry.ChangeFrequency, entry.Priority);
+                }
+            }
+
+            return Content(siteMapBuilder.ToString(), "text/xml");
+        }
+    }
+}
---
+++ src/HeadlessArchitect.Website/Controls/SitemapXmlBuilder.cs
@@ -0,0 +1,84 @@
+﻿namespace HeadlessArchitect.Website.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    public class SitemapXmlBuilder
+    {
+        private readonly XNamespace _namespace = "http://www.sitemaps.org/schemas/sitemap/0.9";
+        private readonly List<SitemapUrl> _urls = new();
+        
+        public void AddUrl(
+            string url, 
+            DateTime? modified = null, 
+            ChangeFrequency? changeFrequency = null, 
+            double? priority = null)
+        {
+            _urls.Add(new SitemapUrl()
+            {
+                Url = url,
+                Modified = modified,
+                ChangeFrequency = changeFrequency,
+                Priority = priority,
+            });
+        }
+
+        public override string ToString()
+        {
+            return new XDocument(
+                    new XDeclaration("1.0", "utf-8", "yes"),
+                    new XElement(_namespace + "urlset",
+                        from item in _urls
+                        select CreateItemElement(item)
+                    )).ToString();            
+        }
+
+        private XElement CreateItemElement(SitemapUrl sitemapUrl)
+        {
+            XElement element = new XElement(_namespace + "url", 
+                new XElement(_namespace + "loc", sitemapUrl.Url.ToLower()));
+
+            if (sitemapUrl.Modified.HasValue)
+            {
+                element.Add(new XElement(_namespace + "lastmod", 
+                    sitemapUrl.Modified.Value.ToString("yyyy-MM-ddTHH:mm:ss.f") + "+00:00"));
+            }
+
+            if (sitemapUrl.ChangeFrequency.HasValue)
+            {
+                element.Add(new XElement(_namespace + "changefreq", 
+                    sitemapUrl.ChangeFrequency.Value.ToString().ToLower()));
+            }
+
+            if (sitemapUrl.Priority.HasValue)
+            {
+                element.Add(new XElement(_namespace + "priority", 
+                    sitemapUrl.Priority.Value.ToString("N1")));
+            }
+
+            return element;
+        }
+    }
+    
+    public enum ChangeFrequency
+    {
+        Undefined,
+        Always,
+        Hourly,
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly,
+        Never
+    }
+
+    public class SitemapUrl
+    {
+        public string Url { get; set; }
+        public DateTime? Modified { get; set; }
+        public ChangeFrequency? ChangeFrequency { get; set; }
+        public double? Priority { get; set; }
+    }
+}
