a2da8c2bfbb8104d52c0edc1fb30139ed63e9a32
commodore73 <git@c73.me>
2021-07-08 11:49:40 -0700

    entry model for generic page content type

---
+++ src/HeadlessArchitect.Core.Models/Models/ContentTypes/Page.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections.Generic;
+
+namespace HeadlessArchitect.Core.Models.ContentTypes
+{
+    using System.Text.Json.Serialization;
+
+    using Deliverystack.Core.Attributes;
+    using Deliverystack.Html;
+    using Deliverystack.Markdown;
+    using Deliverystack.StackContent.Models.Entries;
+
+    [ContentTypeIdentifier("page")]
+    public class Page : ContentstackWebPageEntryModelBase
+    {
+        [JsonPropertyName("firstmarkup")]
+        public MarkupField FirstMarkup { get; set; }
+        
+        [OpenExternalsInNewTabs(true)]
+        [JsonPropertyName("secondmarkup")]
+        public MarkupField SecondMarkup { get; set; } 
+        
+        [JsonPropertyName("markdown")]
+        public List<MarkdownField> Markdown { get; set; }
+//        public string Markdown { get; set; }
+    }
+}
