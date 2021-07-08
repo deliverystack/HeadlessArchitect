8d03d07e24fa73193099558966abc31035663238
commodore73 <git@c73.me>
2021-07-08 11:49:16 -0700

    entry model for home page content type

---
+++ src/HeadlessArchitect.Core.Models/Models/ContentTypes/HomePage.cs
@@ -0,0 +1,13 @@
+﻿namespace HeadlessArchitect.Core.Models.ContentTypes
+{
+    using Deliverystack.Core.Attributes;
+    using Deliverystack.StackContent.Models.Entries;
+    using Deliverystack.StackContent.Models.Fields;
+    using System.Collections.Generic;
+
+    [ContentTypeIdentifier("homepage")]
+    public class HomePage : ContentstackWebPageEntryModelBase
+    {
+        public List<ReferenceField> Reference { get; set; }
+    }
+}
